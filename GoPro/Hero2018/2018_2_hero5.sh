8879649306189f42aabcaa26976e3513b8529a67
Erwin Ried <1091420+eried@users.noreply.github.com>
2018-09-23 20:31:03 +0200

    Update 2018_2_hero5.sh

--- GoPro/Hero2018/2018_2_hero5.sh
+++ GoPro/Hero2018/2018_2_hero5.sh
@@ -7,7 +7,8 @@ rm UPDATE.zip
 printf "CAMLOAD:0\nCAMERA:1\nHIBER:1" > UPDATE/fwupdate.txt
 rm UPDATE/hd5_update.txt
 echo "Download h5to2018.diff from https://gethypoxic.com/blogs/technical/latest-gopro-hero-is-actually-a-gopro-hero5"
-echo -ne "Place diff file in $(pwd) and press enter"; read
+echo -ne "Place diff file in $(pwd) and press enter"
+read
 bspatch UPDATE/camera_firmware.bin UPDATE/camera_patch.bin h5to2018.diff 
 mv UPDATE/camera_patch.bin UPDATE/camera_firmware.bin 
 tree
