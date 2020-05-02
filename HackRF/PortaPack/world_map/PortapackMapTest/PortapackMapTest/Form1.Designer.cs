1e68503f7acc779b931ea37f59b54d18b2587e71
Erwin Ried <1091420+eried@users.noreply.github.com>
2020-05-02 14:04:08 +0200

    Coordinates test

---
+++ HackRF/PortaPack/world_map/PortapackMapTest/PortapackMapTest/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace PortapackMapTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(438, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 260);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 186);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(165, 243);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 91);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "load old";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "lat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "long";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(203, 455);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 91);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "load new";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(283, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "+N -S";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(288, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "+E -W";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(54, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 47);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "home";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(150, 46);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 47);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Easter";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(272, 46);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 47);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Liberty";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(704, 32);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(792, 920);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1542, 1203);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ HackRF/PortaPack/world_map/PortapackMapTest/PortapackMapTest/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace PortapackMapTest
+{
+    public partial class Form1 : Form
+    {
+        private ImageMap old, newimg;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Environment.CurrentDirectory = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+
+            old = new ImageMap("world_map.jpg");
+            newimg = new ImageMap("hackrf6.jpg");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            update_map(double.Parse(textBox2.Text), double.Parse(textBox1.Text), old);
+            pictureBox2.Image = pictureBox1.Image;
+        }
+
+        // Using EQUIDISTANT projection
+        private void update_map(double lon, double lat, ImageMap im)
+        {
+            double lon_ = lon;
+            double lat_ = lat;
+
+            Rectangle map_rect = pictureBox1.DisplayRectangle;
+
+            // Map is in Equidistant "Plate Carrée" projection
+            double x_pos = im.map_center_x - (map_rect.Width / 2) + (lon_ / im.lon_ratio);
+            double y_pos = im.map_center_y - (map_rect.Height / 2) + (lat_ / im.lat_ratio) + 16;
+
+            // Cap position
+            /* if (x_pos > (map_width - map_rect.width()))
+                 x_pos = map_width - map_rect.width();
+             if (y_pos > (map_height + map_rect.height()))
+                 y_pos = map_height - map_rect.height(); */
+
+            var r = new Rectangle((int)x_pos, (int)y_pos, map_rect.Width, map_rect.Height);
+            pictureBox1.Image = ((Bitmap)im.Img).Clone(r,im.Img.PixelFormat);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "-27.1127";
+            textBox2.Text = "-109.3497";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "69.6492";
+            textBox2.Text = "18.9553";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Update_new_map(double.Parse(textBox2.Text), double.Parse(textBox1.Text), newimg);
+            pictureBox2.Image = pictureBox1.Image;
+        }
+
+        // Using WGS 84/Pseudo-Mercator projection
+        private void Update_new_map(double lon, double lat, ImageMap im)
+        {
+            double lon_ = lon;
+            double lat_ = lat;
+
+            Rectangle map_rect = pictureBox1.DisplayRectangle;
+
+            // Map mercator projection
+            //double x_pos = im.map_center_x - (map_rect.Width / 2) + (lon_ / im.lon_ratio);
+            //double y_pos = im.map_center_y - (map_rect.Height / 2) + (lat_ / im.lat_ratio) + 16;
+
+            double x_pos2 = im.map_center_x - (map_rect.Width / 2) + (lon_ / im.lon_ratio);
+
+            // https://stackoverflow.com/questions/14329691/convert-latitude-longitude-point-to-a-pixels-x-y-on-mercator-projection
+            double x_pos = (lon + 180) * ((im.map_center_x * 2) / 360) - (map_rect.Width / 2);
+
+            //double latRad = lat * Math.PI / 180;
+            //double mercN = Math.Log(Math.Tan((Math.PI / 4) + (latRad / 2)));
+            //double y_pos = (im.map_center_y) - (lat * (im.map_center_y * 2)) / 180 - (map_rect.Height / 2);
+
+            //-3E-05x3 + 0.0002x2 - 0.2625x + 49.848
+            double y_pos = im.map_center_y / 50 * (-3e-5 * Math.Pow(lat, 3) + 0.0002 * Math.Pow(lat, 2) -0.2625 * lat + 49.848) - (map_rect.Height / 2);
+
+            // Cap position
+            /* if (x_pos > (map_width - map_rect.width()))
+                 x_pos = map_width - map_rect.width();
+             if (y_pos > (map_height + map_rect.height()))
+                 y_pos = map_height - map_rect.height(); */
+
+            var r = new Rectangle((int)x_pos, (int)y_pos, map_rect.Width, map_rect.Height);
+            pictureBox1.Image = ((Bitmap)im.Img).Clone(r, im.Img.PixelFormat);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "40.6892";
+            textBox2.Text = "-74.0445";
+        }
+    }
+    internal class ImageMap
+    {
+        public ImageMap(string path)
+        {
+            Img = Image.FromFile(path,false);
+            map_center_x = Img.Width / 2;
+            map_center_y = Img.Height / 2;
+            lon_ratio = 180.0 / map_center_x;
+            lat_ratio = -90.0 / map_center_y;
+        }
+
+        public Image Img { get; private set; }
+        public double map_center_x { get; }
+        public double map_center_y { get; }
+        public double lon_ratio { get; }
+        public double lat_ratio { get; }
+    }
+}
