d1e39ecd1737d14b712deb1cf82a5b8a10d198d4
Erwin Ried <1091420+eried@users.noreply.github.com>
2020-05-06 16:16:36 +0200

    World map tester adjust

--- HackRF/PortaPack/world_map/PortapackMapTest/PortapackMapTest/Form1.Designer.cs
+++ HackRF/PortaPack/world_map/PortapackMapTest/PortapackMapTest/Form1.Designer.cs
@@ -41,38 +41,55 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button8 = new System.Windows.Forms.Button();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(438, 32);
+            this.pictureBox1.Location = new System.Drawing.Point(239, 17);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(240, 260);
+            this.pictureBox1.Size = new System.Drawing.Size(132, 142);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(165, 186);
+            this.textBox1.Location = new System.Drawing.Point(90, 101);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.Size = new System.Drawing.Size(56, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(165, 243);
+            this.textBox2.Location = new System.Drawing.Point(90, 132);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 29);
+            this.textBox2.Size = new System.Drawing.Size(56, 20);
             this.textBox2.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(203, 333);
+            this.button1.Location = new System.Drawing.Point(111, 180);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(169, 91);
+            this.button1.Size = new System.Drawing.Size(92, 49);
             this.button1.TabIndex = 3;
             this.button1.Text = "load old";
             this.button1.UseVisualStyleBackColor = true;
@@ -81,26 +98,29 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(82, 186);
+            this.label1.Location = new System.Drawing.Point(45, 101);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 25);
+            this.label1.Size = new System.Drawing.Size(18, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "lat";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(49, 243);
+            this.label2.Location = new System.Drawing.Point(27, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 25);
+            this.label2.Size = new System.Drawing.Size(27, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "long";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(203, 455);
+            this.button2.Location = new System.Drawing.Point(111, 246);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(169, 91);
+            this.button2.Size = new System.Drawing.Size(92, 49);
             this.button2.TabIndex = 6;
             this.button2.Text = "load new";
             this.button2.UseVisualStyleBackColor = true;
@@ -109,26 +129,29 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(283, 185);
+            this.label3.Location = new System.Drawing.Point(154, 100);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.Size = new System.Drawing.Size(34, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "+N -S";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(288, 243);
+            this.label4.Location = new System.Drawing.Point(157, 132);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 25);
+            this.label4.Size = new System.Drawing.Size(37, 13);
             this.label4.TabIndex = 8;
             this.label4.Text = "+E -W";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(54, 46);
+            this.button3.Location = new System.Drawing.Point(29, 25);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 47);
+            this.button3.Size = new System.Drawing.Size(41, 25);
             this.button3.TabIndex = 9;
             this.button3.Text = "home";
             this.button3.UseVisualStyleBackColor = true;
@@ -136,9 +159,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(150, 46);
+            this.button5.Location = new System.Drawing.Point(82, 25);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(90, 47);
+            this.button5.Size = new System.Drawing.Size(49, 25);
             this.button5.TabIndex = 9;
             this.button5.Text = "Easter";
             this.button5.UseVisualStyleBackColor = true;
@@ -146,9 +170,10 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(272, 46);
+            this.button6.Location = new System.Drawing.Point(148, 25);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(100, 47);
+            this.button6.Size = new System.Drawing.Size(55, 25);
             this.button6.TabIndex = 9;
             this.button6.Text = "Liberty";
             this.button6.UseVisualStyleBackColor = true;
@@ -157,18 +182,170 @@
             // pictureBox2
             // 
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Location = new System.Drawing.Point(704, 32);
+            this.pictureBox2.Location = new System.Drawing.Point(384, 17);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(792, 920);
+            this.pictureBox2.Size = new System.Drawing.Size(433, 499);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 10;
             this.pictureBox2.TabStop = false;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(48, 56);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "istambul";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(207, 246);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(92, 49);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "load new bigger";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 3;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.numericUpDown1.Location = new System.Drawing.Point(90, 347);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 13;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 3;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.numericUpDown2.Location = new System.Drawing.Point(90, 373);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 13;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 3;
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.numericUpDown3.Location = new System.Drawing.Point(90, 399);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 13;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(265, 410);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(21, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "x";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button8_MouseDown);
+            this.button8.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button8_MouseMove);
+            this.button8.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button8_MouseUp);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.DecimalPlaces = 3;
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.numericUpDown4.Location = new System.Drawing.Point(90, 425);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 13;
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.DecimalPlaces = 2;
+            this.numericUpDown5.Location = new System.Drawing.Point(90, 451);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown5.TabIndex = 13;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1542, 1203);
+            this.ClientSize = new System.Drawing.Size(841, 652);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -182,10 +359,16 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -206,6 +389,14 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
     }
 }
 
--- HackRF/PortaPack/world_map/PortapackMapTest/PortapackMapTest/Form1.cs
+++ HackRF/PortaPack/world_map/PortapackMapTest/PortapackMapTest/Form1.cs
@@ -15,7 +15,8 @@ namespace PortapackMapTest
 {
     public partial class Form1 : Form
     {
-        private ImageMap old, newimg;
+        private ImageMap old, newimg, newimg2;
+        private bool _drag;
 
         public Form1()
         {
@@ -24,6 +25,7 @@ namespace PortapackMapTest
 
             old = new ImageMap("world_map.jpg");
             newimg = new ImageMap("hackrf6.jpg");
+            newimg2 = new ImageMap("hackrf7.jpg");
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -84,26 +86,58 @@ namespace PortapackMapTest
             //double x_pos = im.map_center_x - (map_rect.Width / 2) + (lon_ / im.lon_ratio);
             //double y_pos = im.map_center_y - (map_rect.Height / 2) + (lat_ / im.lat_ratio) + 16;
 
-            double x_pos2 = im.map_center_x - (map_rect.Width / 2) + (lon_ / im.lon_ratio);
-
             // https://stackoverflow.com/questions/14329691/convert-latitude-longitude-point-to-a-pixels-x-y-on-mercator-projection
             double x_pos = (lon + 180) * ((im.map_center_x * 2) / 360) - (map_rect.Width / 2);
 
-            //double latRad = lat * Math.PI / 180;
-            //double mercN = Math.Log(Math.Tan((Math.PI / 4) + (latRad / 2)));
-            //double y_pos = (im.map_center_y) - (lat * (im.map_center_y * 2)) / 180 - (map_rect.Height / 2);
+           
+            double map_height = im.map_center_y * (2);
 
-            //-3E-05x3 + 0.0002x2 - 0.2625x + 49.848
-            double y_pos = im.map_center_y / 50 * (-3e-5 * Math.Pow(lat, 3) + 0.0002 * Math.Pow(lat, 2) -0.2625 * lat + 49.848) - (map_rect.Height / 2);
+            double map_bottom = Math.Sin((-85+(double)numericUpDown1.Value )* Math.PI / 180);
 
-            // Cap position
-            /* if (x_pos > (map_width - map_rect.width()))
-                 x_pos = map_width - map_rect.width();
-             if (y_pos > (map_height + map_rect.height()))
-                 y_pos = map_height - map_rect.height(); */
+                double lat_rad = Math.Sin(lat* Math.PI / 180 );
+                double map_world_lon = (im.map_center_x * (2.0 + (double)numericUpDown2.Value)) / (2 * Math.PI);
+                double mapOffsetY = (map_world_lon / 2 * Math.Log((1 + map_bottom) / (1 - map_bottom)));
 
-            var r = new Rectangle((int)x_pos, (int)y_pos, map_rect.Width, map_rect.Height);
+            double y_pos = map_height - ((map_world_lon / 2 * Math.Log((1 + lat_rad) / (1 - lat_rad))) - mapOffsetY) - (double)numericUpDown5.Value;
+
+
+
+        // Cap position
+        /* if (x_pos > (map_width - map_rect.width()))
+             x_pos = map_width - map_rect.width();
+         if (y_pos > (map_height + map_rect.height()))
+             y_pos = map_height - map_rect.height(); */
+
+        var r = new Rectangle((int)x_pos, (int)y_pos, map_rect.Width, map_rect.Height);
             pictureBox1.Image = ((Bitmap)im.Img).Clone(r, im.Img.PixelFormat);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "40.843611";
+            textBox2.Text = "29.288333";
+        }
+
+        private void button8_MouseDown(object sender, MouseEventArgs e)
+        {
+            _drag = true;
+        }
+
+        private void button8_MouseUp(object sender, MouseEventArgs e)
+        {
+            _drag = false;
+        }
+
+        private void button8_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_drag)
+                button8.Location = this.PointToClient(Cursor.Position);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Update_new_map(double.Parse(textBox2.Text), double.Parse(textBox1.Text), newimg2);
+            pictureBox2.Image = pictureBox1.Image;
         }
 
         private void button6_Click(object sender, EventArgs e)
