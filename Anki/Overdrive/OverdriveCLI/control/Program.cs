0588cd08c9606325f33ae50162448590fa40df73
Erwin Ried <1091420+eried@users.noreply.github.com>
2020-05-22 00:34:04 +0200

    OverdriveCLI

---
+++ Anki/Overdrive/OverdriveCLI/control/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Buffers;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+
+namespace control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = BluetoothLEDevice.FromBluetoothAddressAsync(Convert.ToUInt64(args[0].Replace(":", ""), 16));
+            while (r.Status != Windows.Foundation.AsyncStatus.Completed);
+            var device = r.GetResults();
+            Console.WriteLine("Connected to: " +device.Name);
+
+            //var s = device.GetGattService(new Guid("be15bee06186407e83810bd89c4d8df4"));
+
+            //var t = s.OpenAsync(Windows.Devices.Bluetooth.GenericAttributeProfile.GattSharingMode.Exclusive);
+
+            //while (t.Status != Windows.Foundation.AsyncStatus.Completed);
+            //s.GetCharacteristics(new Guid("be15bee06186407e83810bd89c4d8df4"))[0].wri
+        }
+    }
+}
---
+++ Anki/Overdrive/OverdriveCLI/scan/scan/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Windows.Devices.Bluetooth.Advertisement;
+
+namespace scan
+{
+    class Program
+    {
+        private static HashSet<ulong> Found;
+        private static BluetoothLEAdvertisementWatcher Watcher;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Waiting for BLE devices.");
+
+            Found = new HashSet<ulong>();
+            Watcher = new BluetoothLEAdvertisementWatcher { ScanningMode = BluetoothLEScanningMode.Active };
+            Watcher.Received += DeviceFound;
+            Watcher.Start();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        public static string MAC802DOT3(ulong macAddress)
+        {
+            return string.Join(":",
+                                BitConverter.GetBytes(macAddress).Reverse()
+                                .Select(b => b.ToString("X2"))).Substring(6);
+        }
+
+        private static void DeviceFound(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            if (!Found.Contains(args.BluetoothAddress))
+            {
+                var n = args.Advertisement.LocalName;
+                Found.Add(args.BluetoothAddress);
+                Console.WriteLine("Address: " + MAC802DOT3(args.BluetoothAddress) + " - Name: " + (string.IsNullOrEmpty(n) ? "(Unknown)" : n));
+            }
+        }
+    }
+}
